798ac27d2e1fdb737b1ed8ddf70c1d7f76869ff1
Matthew Davey <reapazor@gmail.com>
2017-06-10 15:07:27 -0400

    Release script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ cd "$(dirname "$0")"
 go build -o bin/jarvis ./src
 GOOS=windows GOARCH=386 go build -o bin/jarvis.exe ./src
 rm -rf deploy/
+rm -rf jarvis.zip
 mkdir -p deploy
 cp -rf bin/jarvis deploy/
 cp -rf bin/jarvis.exe deploy/
 cp -rf resources/jarvis.toml deploy/
+zip -r jarvis.zip deploy/
+rm -rf deploy/
