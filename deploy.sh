06ea003fc45ace4751ac20c898d4b0a929d00fc2
Matthew Davey <reapazor@gmail.com>
2017-06-21 17:01:56 -0400

    Fix build script

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,25 @@ cd "$(dirname "$0")"
 # Remove & Create Temp Folder
 rm -rf jarvis-build/
 mkdir -p jarvis-build
-mkdir -p jarvis-build/www
+
 
 # Build OSX
-cp -rf resources/build/macos/JARVIS.app jarvis-build/
-cp -rf resources/jarvis.json jarvis-build/JARVIS.app/Contents/MacOS/
-go build -o jarvis-build/JARVIS.app/Contents/MacOS/jarvis ./src
+mkdir -p jarvis-build/macOS
+cp -rf resources/build/macos/JARVIS.app jarvis-build/macOS
+cp -rf resources/jarvis.json jarvis-build/macOS/JARVIS.app/Contents/MacOS/
+go build -o jarvis-build/macOS/JARVIS.app/Contents/MacOS/jarvis ./src
+cp -rf resources/www jarvis-build/macOS/JARVIS.app/Contents/MacOS/
 
 # Build For Windows
+mkdir -p jarvis-build/windows
 rsrc -manifest resources/build/windows/jarvis.exe.manifest -ico resources/build/windows/jarvis.ico -o src/jarvis.syso
-GOOS=windows GOARCH=386 go build -o jarvis-build/jarvis.exe ./src
+GOOS=windows GOARCH=386 go build -o jarvis-build/windows/jarvis.exe ./src
 rm -rf src/jarvis.syso
+cp -rf resources/jarvis.json jarvis-build/windows/
+cp -rf resources/www jarvis-build/windows/
 
 # Remove Previous Package
-rm -rf jarvis.zip
-
-# Copy Resources Over
-cp -rf resources/jarvis.json jarvis-build/
-cp -rf resources/www jarvis-build/
+rm -rf jarvis-build.zip
 
 # Copy Over Instructions
 cp -rf README.md jarvis-build/
